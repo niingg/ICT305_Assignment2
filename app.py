0eb165a06690b1ad7dc957e9c47199e2d87f7fbf
cellWall <salwasanaullahkhan@gmail.com>
2025-11-08 21:23:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -214,10 +214,10 @@ if page == "Introduction":
     **The Problem:**
     Despite the easy accessibility and availability of healthcare data, there are few resources that have translated complex datasets into meaningful insights.
              
-    **Our Goal**
+    **Our Goal:**
     This dashboard was developed to present facts and figures about risk factors associated with diabetes in an engaging and digestible manner, even for users with minimal medical knowledge.
 
-    **The Narrative**         
+    **The Narrative:**         
     The progression of hypotheses in this dashboard follows the level of impact they have on the potential development of diabetes, in the order of least to most impactful. The conclusion summarises main takeaways from the dashboard and provides actionable recommendations for all target audiences.
     """)
 
